d04439110b2b1f4f91ad8f8853d370e67a2d4d59
s6r6k <s6r6kh6n@gmail.com>
2026-07-03 15:04:05 +0700

    Time: 23 ms (80.51%), Space: 13.3 MB (28.86%) - LeetHub

--- 0055-jump-game/0055-jump-game.py
+++ 0055-jump-game/0055-jump-game.py
@@ -4,14 +4,15 @@ class Solution(object):
         :type nums: List[int]
         :rtype: bool
         """
-        farthest = 0
+        farthestIndex = 0
         for i in range(len(nums)):
-            if i > farthest:
+            if i > farthestIndex:
                 return False
-            farthest = max(farthest, i + nums[i])
-            if(farthest >= len(nums) - 1):
+            farthestIndex = max(farthestIndex, i + nums[i])
+            if(farthestIndex >= len(nums) - 1):
                 return True
         return True
+#My initial focus was so much on the path and counting along so i was following individual paths and that is so confusing and time ocmplexity high. focus needs to be shifted to each index, whats the furthest index i can go to overall? that is a global variable.
 
 
         
